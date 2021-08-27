82388ca280026327b288309a8c37862c28531d3a
Herebus <jairoam@gmail.com>
2021-08-27 14:04:44 -0500

    Changes and correct errors

--- Exercises/module2.ts
+++ Exercises/module2.ts
@@ -1,19 +1,19 @@
 /*
 Intersection type example
 */
-interface Employee {
+interface Employee3 {
     employeeID: number;
     age: number;
   }
   interface Manager {
     stockPlan: boolean;
   }
-  type ManagementEmployee = Employee & Manager;
+ /*  type ManagementEmployee = Employee & Manager;
   let newManager: ManagementEmployee = {
       employeeID: 12345,
       age: 34,
       stockPlan: true
-  };
+  }; */
 
   //Definition of literal types
     type testResult = "pass" | "fail" | "incomplete";
